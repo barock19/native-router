814c47e1162088af218d9e3ac7214383b786fdd8
Zidni Mubarock <zidmubarock@gmail.com>
2016-04-09 23:56:03 +0700

    route matcher

---
+++ src/services/RouteMatcher.js
@@ -0,0 +1,61 @@
+import {Map} from "immutable";
+import {RouteStack} from "reducers/Navigation";
+
+function routeToSegment(route) {
+  let segments = route.split('/').filter( seg => seg != '')
+  let patterns = segments.filter(seg => seg.match(/^\:(\w+)/))
+  let length = segments.length
+
+  return {segments, patterns,length}
+}
+
+const stringPatternHandler = (routes, route)=>{
+  let {segments, length} = routeToSegment(route)
+  let patternToValueMap = {}
+
+  let foundTarget = routes
+    .map( route => { return {route, _segmentInfo: routeToSegment(route.path)} })
+    .find( (item) => {
+      if(item._segmentInfo.length != length)
+        return false;
+
+      if(item.route.path == route)
+        return true;
+
+      let ASegments = segments
+      let BSegments =  item._segmentInfo.segments
+      let BSegmentsPatterns = item._segmentInfo.patterns
+
+      let possiblyFoundedPattern = []
+      ASegments.forEach((ASegment, i)=>{
+        let BSegment = BSegments[i]
+        if(ASegment != BSegment){
+          possiblyFoundedPattern.push(BSegment)
+          patternToValueMap[BSegment.replace(':', '')] =  ASegment
+        }
+      })
+      return BSegmentsPatterns.join('-') == possiblyFoundedPattern.join('-')
+    })
+
+  if(!foundTarget)
+    return false;
+  else {
+    return Object.assign({}, foundTarget.route, {path: route, params: new Map(patternToValueMap)})
+  }
+}
+
+const stringHandler = (routes, route)=>{
+  let foundTarget = stringPatternHandler(routes, route)
+  return foundTarget ?  new RouteStack(foundTarget) : false
+}
+const recordHandler = (routes, route)=>{
+}
+
+export default (routes, route)=> {
+  if(typeof(route) == 'string')
+    return stringHandler(routes, route);
+  else if (route instanceof RouteStack)
+    return recordHandler(routes, route);
+  else
+    return false;
+}
---
+++ test/services/RouteMatcher__test.js
@@ -0,0 +1,58 @@
+import React,{Component} from "react";
+import {Map} from "immutable";
+import RouteMatcher from "services/RouteMatcher"
+import {RouteStack} from "reducers/Navigation"
+
+class Home              extends Component{render(){ return null }}
+class UserCollection    extends Component{render(){ return null }}
+class UserDetail        extends Component{render(){ return null }}
+class UserContents      extends Component{render(){ return null }}
+class UserContentDetail extends Component{render(){ return null }}
+
+describe.only("Services/RouteMatcher",  () => {
+  const sampleRoutes = [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/users',
+      component: UserCollection
+    },
+    {
+      path: '/users/:id',
+      component: UserDetail
+    },
+    {
+      path: '/users/:id/contents',
+      component: UserContents,
+    },
+    {
+      path: '/users/:id/contents/:content_id',
+      component: UserContentDetail
+    }
+  ]
+
+  describe("when `route` given as string", () => {
+    it("should find the correct route", () => {
+      expect(RouteMatcher(sampleRoutes, '/')).to
+        .be.eq(new RouteStack({path: '/', component: Home}) )
+      expect(RouteMatcher(sampleRoutes, '/users')).to
+        .be.eq(new RouteStack({path: '/users', component: UserCollection}) )
+    });
+
+    it("should find the route with path pattern", () => {
+      expect(RouteMatcher(sampleRoutes, '/users/1')).to
+        .be.eq(new RouteStack({path: '/users/1', component: UserDetail, params: new Map({id: '1'})}) )
+    });
+
+  });
+
+  describe("getting route with predefined pattern", () => {
+    describe("when target passsed as string", () => {
+      it("should find the correct route", () => {
+        expect()
+      });
+    });
+  });
+});
